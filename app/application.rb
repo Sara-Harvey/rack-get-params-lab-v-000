278a02e0b6e2511dd46de1b210e5c0edf2fe0aa7
Sara Harvey <saraharveynyc@gmail.com>
2019-07-25 14:32:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,12 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+      end
+    
     elsif req.path.match(/cart/)
       @@cart.each do |cart_item|      
         resp.write "#{cart_item}\n"
