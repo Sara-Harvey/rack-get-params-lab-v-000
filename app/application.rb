d21f593bb90e715a313bd53615cd3ac933d88377
Sara Harvey <saraharveynyc@gmail.com>
2019-07-25 14:17:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,8 +16,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
       
-    elsif req.path.match(/search/)
-      
+    elsif req.path.match(/cart/)
+            
     
     else
       resp.write "Path Not Found"
