83dc478ed237de306e7778308aa186196cc6b299
Sara Harvey <saraharveynyc@gmail.com>
2019-07-25 14:25:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,10 @@ class Application
         else
           resp.write "Your cart is empty."  
         end
+
+    elsif req.path.match(/add/)
+
+
         
     else
       resp.write "Path Not Found"
